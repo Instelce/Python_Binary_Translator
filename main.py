0eda68dd43a0d8aa84c274d3ca3064c6336331a9
Instelce <intelce.developpement@gmail.com>
2021-04-06 18:13:42 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# Dictionaire des valeurs en base 10
+bin = {
+    '1':'2048',
+    '2': '1024',
+    '3': '512',
+    '4': '256',
+    '5': '128',
+    '6': '64',
+    '7': '32',
+    '8': '16',
+    '9': '8',
+    '10': '4',
+    '11': '2',
+    '12': '1',
+}
+
+while True:
+    index = 0
+    result = 0
+    request_bin = input(' > ')
+    request_split = request_bin.split('.')
+    request_length = len(request_split)
+    index += len(bin) - request_length
+
+    for i in request_split:
+        index += 1
+        if i == '1':
+            result += int(bin.get(str(index)))
+    print(f'\n| {result} \n')
