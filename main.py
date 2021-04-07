93a542cbc2f9db7bd29cef604f2cc81350f07dff
Instelce <intelce.developpement@gmail.com>
2021-04-07 13:35:46 +0200

    Added base 10 to base 2 conversion. Reformulation of the code.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Dictionaire des valeurs en base 10
-base_2 = {
+sequence = {
     '1':'2048',
     '2': '1024',
     '3': '512',
@@ -14,16 +14,39 @@ base_2 = {
     '12': '1',
 }
 
-while True:
-    index = 0
-    result = 0
-    request_bin = input(' > ')
-    request_split = request_bin.split('.')
-    request_length = len(request_split)
-    index += len(base_2) - request_length
+base = '>> '
 
-    for i in request_split:
-        index += 1
-        if i == '1':
-            result += int(base_2.get(str(index)))
-    print(f'\n| {result} \n')
+while True:
+    print('\nChoix de conversion : \n| base 2 en base 10 (1) \n| base 10 en base 2 (2)')
+    trad_choice = input(base)
+
+    # Conversion base 2 à base 10
+    if trad_choice == '1':
+        print('\nConversion base 2 à base 10 \nEx: 0 0 0 0 0 0 0 1 ou 0 0 1 0')
+        index = 0
+        result_2t10 = 0
+        request_2t10 = input(base)
+        request_split = request_2t10.split(' ')
+        request_length = len(request_split)
+        index += len(sequence) - request_length
+
+        for i in request_split:
+            index += 1
+            if i == '1':
+                result_2t10 += int(sequence.get(str(index)))
+        print(f'\n| {result_2t10} \n')
+
+    # Conversion base 10 à base 2
+    elif trad_choice == '2':
+        print('\nConversion base 10 à base 2')
+        result_10t2 = []
+        request_10t2 = int(input(base))
+
+        for i in sequence:
+            if request_10t2 >= int(sequence.get(i)):
+                request_10t2 -= int(sequence.get(i))
+                result_10t2.append('1')
+            else:
+                result_10t2.append('0')
+
+        print('| '+''.join(result_10t2)+'\n')
